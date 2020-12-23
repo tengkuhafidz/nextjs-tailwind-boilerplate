0b5c58ac02694284123a5e472eca8be9451c5d61
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-12-23 22:51:30 +0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Site Title</title>
+      </Head>
+      <h1 className="text-6xl">hello word</h1>
+    </div>
+  )
+}
